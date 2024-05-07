1baec37f14e7eace6a61edb586225d27201d0ff2
FedeZar <zardo.fede@gmail.com>
2024-05-07 20:51:22 +0200

    added MECHA_RAT ASCII art

---
+++ src/main/java/com/tloj/game/utilities/Constants.java
@@ -0,0 +1,26 @@
+package com.tloj.game.utilities;
+
+public class Constants {
+
+    public static final String RAT = 
+    """
+                     .  ,
+                    (\\;/)
+                    oo   \\//,        _
+                  ,/_;~      \\,     / '
+                  "'    (  (   \\    !
+                      //  \\   |__.'
+                      '~  '~----''
+    """;
+    
+
+
+
+
+
+
+
+
+
+    
+}
