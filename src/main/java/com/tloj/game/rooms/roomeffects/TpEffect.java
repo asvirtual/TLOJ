47616e4954557b5bc076a8a62a44e583d8c15f3e
Asvirtual <giordano.alberti@proton.me>
2024-05-08 18:16:55 +0200

    Minor fix

--- src/main/java/com/tloj/game/rooms/roomeffects/TpEffect.java
+++ src/main/java/com/tloj/game/rooms/roomeffects/TpEffect.java
@@ -3,12 +3,10 @@ package com.tloj.game.rooms.roomeffects;
 import com.tloj.game.entities.Character;   
 import com.tloj.game.game.Controller;
 
-public class TpEffect {
-
+public class TpEffect implements RoomEffect {
     @Override
     public void applyEffect(Character character) {
         Controller controller = Controller.getInstance();
-        // TODO tp logic here
     }   
     
 }
