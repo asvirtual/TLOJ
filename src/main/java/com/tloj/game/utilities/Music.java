1d120f699ab93e2a2367477df9207d9c1676e75c
FedeZar <zardo.fede@gmail.com>
2024-05-13 10:07:08 +0200

    added item ascii

---
+++ src/main/java/com/tloj/game/utilities/Music.java
@@ -0,0 +1,50 @@
+package com.tloj.game.utilities;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+import java.io.*;
+
+public class Music {
+    public void playMusic(){
+        try {
+
+        String LOOP = "./musicFile/loop.wav"; 
+        String INTRO = "./musicFile/intro.wav";
+
+        File loopFile = new File(LOOP);
+        File introFile = new File(INTRO);
+        if(loopFile.exists() && introFile.exists()){ 
+                
+            AudioInputStream audioInput = AudioSystem.getAudioInputStream(introFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInput);
+            clip.start();
+            Thread.sleep(clip.getMicrosecondLength());
+            clip.close();
+            audioInput = AudioSystem.getAudioInputStream(loopFile);
+            clip = AudioSystem.getClip();
+            clip.open(audioInput);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            //JOptionPane.showMessageDialog(null,"Press ok to stop playing");
+            }
+        else{
+                System.out.println("Couldn't find Music file");
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void stopMusic(){
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.stop();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
